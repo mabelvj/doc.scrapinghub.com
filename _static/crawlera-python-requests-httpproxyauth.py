8507416b88e0716ea9e895fcb26005956c06429f
Nestor Toledo Koplin <nestor@scrapinghub.com>
2017-04-05 18:33:09 +0200

    Update crawlera-python-requests-httpproxyauth.py

--- _static/crawlera-python-requests-httpproxyauth.py
+++ _static/crawlera-python-requests-httpproxyauth.py
@@ -4,7 +4,8 @@ url = "https://twitter.com"
 proxy_host = "proxy.crawlera.com"
 proxy_port = "8010"
 proxy_auth = "<APIKEY>:"
-proxies = {"https": "https://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)}
+proxies = {"https": "https://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port),
+           "http": "http://{}@{}:{}/".format(proxy_auth, proxy_host, proxy_port)}
 
 r = requests.get(url, proxies=proxies, verify='/path/to/crawlera-ca.crt')
 
